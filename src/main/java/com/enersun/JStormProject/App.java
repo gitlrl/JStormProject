63ba22534f7b66f8ebacdf2647b3ac89c63f8379
SharpLin <3lrl@163.com>
2016-04-08 11:18:16 +0800

    First Version

---
+++ src/main/java/com/enersun/JStormProject/App.java
@@ -0,0 +1,13 @@
+package com.enersun.JStormProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
